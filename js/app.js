3dba98945eb91cfd68431e51988965ab324d826a
blaisethomas <blaisegthomas@gmail.com>
2014-07-19 16:33:30 -0700

    board working, clicks working, player turn alternating, ready for logic

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ var tttApp = angular.module('TicTacToe', []);
 tttApp.controller('tttController',function($scope){
 
 /////////set the board /////////////
-$scope.sizeBox = 3;
+$scope.sizeBox = 13;
 $scope.newBoard = function(size) {
 	$scope.board = [];
 	for (var i=0; i<size; ++i) {
@@ -12,9 +12,11 @@ $scope.newBoard = function(size) {
 		for (var j=0; j<size; ++j) {
 			colset.push({
 				x:(j+1),
-				y:(i+1)
-				//mark:null,
-				// active:false
+				y:(i+1),
+				mark:null,
+				active:false,
+				p1owns:false,
+				p2owns:false
 			});	
 		}
 		$scope.board.push(colset)
@@ -26,27 +28,33 @@ $scope.newBoard($scope.sizeBox);
 
 $scope.playCounter=0;
 
-$scope.count = function(){
-	
-	if ($scope.playCounter%2 == 0) {
-		//alert("player1");
-		$scope.playCounter++;
-		$scope.player1 = 'p1';  
-
-
-	} else {
-		
-
-		//alert("player2");
-		$scope.playCounter++;
-		$scope.player2 = 'p2';
-	}
+$scope.cellClick = function(box){
+	if(box.active == false) {
+		if ($scope.playCounter%2 == 0) {
+			//alert("player1");
+			box.active = true;
+			box.p1owns = true;
+			box.mark = 'X';
+			$scope.playCounter++;
+			// $scope.player1 = 'p1';  
+		} else {
+			//alert("player2");
+			box.active = true;
+			box.p2owns = true;
+			box.mark = 'Y';
+			$scope.playCounter++;
+			$scope.player2 = 'p2';
+		}
+	} 
+	else {
+		alert('taken')
+	}	
 }
 // $scope.clicked = function(x,y){
 // alert(x + " , " + y);
 // }
 
-// var $scope.isCell
+
 
 
 }); //////////////////////////////END OF SCOPE	
