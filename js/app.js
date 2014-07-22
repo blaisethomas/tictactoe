0229e76c85c66808b9d99b044d08e8cc68527fa6
blaisethomas <blaisegthomas@gmail.com>
2014-07-22 12:17:37 -0700

    test

--- js/app.js
+++ js/app.js
@@ -4,15 +4,15 @@ var tttApp = angular.module('TicTacToe', []);
 tttApp.controller('tttController',function($scope){
 
 /////////set the board /////////////
-$scope.sizeBox = 3;
+$scope.sizeBoard = 3;
 $scope.newBoard = function(size) {
 	$scope.board = [];
 	for (var i=0; i<size; ++i) {
 		var boxSet = [];
 		for (var j=0; j<size; ++j) {
-			boxSet.push({
-				x:(j+1),
-				y:(i+1),
+			boxSet.push({ 
+				x:[(j+1),(i+1)],
+				//y:(i+1),
 				mark:null,
 				active:false,
 				p1owns:false,
@@ -22,7 +22,7 @@ $scope.newBoard = function(size) {
 		$scope.board.push(boxSet)
 	}	
 };
-$scope.newBoard($scope.sizeBox);
+$scope.newBoard($scope.sizeBoard);
 
 ////////////CLICK//////////////////
 
@@ -34,31 +34,80 @@ $scope.cellClick = function(box){
 			//alert("player1");
 			box.active = true;
 			box.p1owns = true;
-			box.mark = 'X';
+			//box.mark = 'X';
 			$scope.playCounter++;
-			playerOne.push({
-				x:box.x,
-				y:box.y 
-			})
+			playerOne.push([box.x]);
 			// $scope.player1 = 'p1'; 
 			//push to new array or object called playerOne 
 		} else {
 			//alert("player2");
 			box.active = true;
 			box.p2owns = true;
-			box.mark = 'Y';
+			//box.mark = 'Y';
 			$scope.playCounter++;
+			playerTwo.push([box.x]);
 			//$scope.player2 = 'p2';
 		}
-	} 
-	else {
+
+	} else {
 		alert('taken')
-	}	
+	}
 }
 
-playerOne = [] ;
+$scope.winCheck = function () {
+	if ($scope.playCounter > 8) {
+	var results = playerOne.map(function(arrayCell){
+		console.log(playerOne + "\n" + playerTwo);
+		});
+	} else {
+		console.log("playing")
+	}
+}
 
- 
+playerOne = [];
+playerTwo = [];
+
+// if (arrayCell == "x: 2, y: 2") {
+// 			alert("winner!");
+// 		} else {
+// 			alert("searching")
+// 		}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $scope.winCheck(playerOne);
+// $scope.winCheck(playerTwo)
+// if (playCounter >=4 ) {
+// for  {
+// 	
+// } 
+// }
+
+ // $scope.winCheck = function(arrayCount){
+// 			while ($scope.playCounter < 9) {
+// 				if (arrayCount == "x: 2, y: 2") {
+// 				alert("p1wins!!");
+// 				} else {
+// 				alert("debug");
+// 				}
+// 			}
+// 		}
+// }	
 
 
 
